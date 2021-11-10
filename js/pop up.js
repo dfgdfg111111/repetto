db0aea29dc623050a2f690ffce5a85308f96a61d
dfgdfg111111 <llagirl920202@gmail.com>
2021-11-10 17:05:55 +0900

    레페토 사이트

---
+++ js/pop up.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){  
+    //모바일 크기로 새창 띄우기
+ 
+     function mobileOpen(주소){
+         window.open("http://","반응형2",
+         'width=360; heiht=200')
+};
+    
+   //window.open("주소","팝업이름","속성");   
+    
+    
